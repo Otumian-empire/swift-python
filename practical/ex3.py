74d8994dfd08ba1957956b587a069f342be3f91f
otumian <popecan1000@gmail.com>
2020-05-29 17:11:43 +0000

    practicals - exercise3

---
+++ practical/ex3.py
@@ -0,0 +1,6 @@
+# In Exercise 2 (Data types) , add comments to your solutions to give the 
+# why/what the line is doing or meant to do ( remember that not all lines 
+# needs comment if the code is discriptive itself).
+
+# max_width is the maximum width of each field
+max_width = 34.55
