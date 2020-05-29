e96aea5c96ce4352954b0a13e09047407ef20936
otumian <popecan1000@gmail.com>
2020-05-29 17:00:19 +0000

    practicals - exercise 0

---
+++ practical/ex1.py
@@ -0,0 +1,20 @@
+# Create 10 variables and assign them any values suitable ( perhaps, as above).
+
+age = 20
+name = "tuna"
+
+speed_limit = "120km/h"
+average = 2.34
+
+size = 23
+number_of_babies = 2
+
+number_of_scripts = 100
+file_writer = "this is a file writer"
+
+dob = '31/13/3001'
+date_of_birth = '32nd Minuamber, 1963'
+birth_date = 'April 1, 1976'
+
+person_1 = "daniel"
+person_2 = "anthony"
