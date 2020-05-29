e1476058d68ef8e501048dd1a000987b9e4e0050
otumian <popecan1000@gmail.com>
2020-05-29 17:08:42 +0000

    practicals - exercise 2

---
+++ practical/ex2.py
@@ -0,0 +1,40 @@
+# 1. Look into Exercise 1 ( creating a variable) , and for the 10 varaibles we created earlier, state their data types
+
+age = 20  # integer
+name = "tuna"  # string
+
+speed_limit = "120km/h"  # string
+average = 2.34  # float
+
+size = 23  # integer
+number_of_babies = 2  # integer
+
+number_of_scripts = 100  # integer
+file_writer = "this is a file writer"  # string
+
+dob = '31/13/3001'  # string
+date_of_birth = '32nd Minuamber, 1963'  # string
+birth_date = 'April 1, 1976'  # string
+
+person_1 = "daniel"  # string
+person_2 = "anthony"  # string
+
+#  2. What will be the data type of the following values:
+# 'University of github' -> string
+# 'swift-Python' -> string
+#  100 -> integer
+#  12.0 -> float
+#  12 -> integer
+# 'B+' -> string
+#  '1 + 1' -> string
+#  345.0 -> float
+#  360 -> integer
+#  3.1432 -> float
+#  "sipping ice cream from a linux cup" -> string
+
+# 3. Now create a variable with a suitable name 
+# ( the name of the variable should be in a way that describes 
+# or gives an information about what the value of interest is) 
+# Eg: user_ip = '123.123.100.134' and user_name = 'John Doe'
+
+max_width = 34.55
